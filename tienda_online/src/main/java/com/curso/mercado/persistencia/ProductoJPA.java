b1f6b892d9fd704026bf7247e9c8aa285a29f616
markelillo <markeletxebarria77@gmail.com>
2022-10-28 01:37:07 +0200

    tienda online JPA Producto

--- tienda_online/src/main/java/com/curso/mercado/persistencia/ProductoJPA.java
+++ tienda_online/src/main/java/com/curso/mercado/persistencia/ProductoJPA.java
@@ -1,6 +1,12 @@
 package com.curso.mercado.persistencia;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.curso.mercado.entidades.Producto;
 
@@ -8,14 +14,38 @@ public class ProductoJPA implements GenericDAO<Producto> {
 
 	@Override
 	public void add(Producto entidad) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("OracleHRPU");
+		EntityManager em = factory.createEntityManager();
+		Producto pNuevo = new Producto();
+		pNuevo.setIdProducto(entidad.getIdProducto());
+		pNuevo.setDescripcion(entidad.getDescripcion());
+		pNuevo.setPrecio(entidad.getPrecio());
+		em.getTransaction().begin();
+		em.persist(pNuevo);
+		
+		em.getTransaction().commit();
 		
 	}
 
 	@Override
 	public List<Producto> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("OracleHRPU");
+		EntityManager em = factory.createEntityManager();
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		
+		Query query = em.createQuery("select p from Producto p");
+		List<Producto> lista =  query.getResultList();
+		
+		for (Producto prod : lista) {
+			Producto p = new Producto();
+			p.setIdProducto(prod.getIdProducto());
+			p.setDescripcion(prod.getDescripcion());
+			p.setPrecio(prod.getPrecio());
+			p.setStock(prod.getStock());
+			productos.add(p);
+			
+		}
+		return productos;
 	}
 
 	@Override
