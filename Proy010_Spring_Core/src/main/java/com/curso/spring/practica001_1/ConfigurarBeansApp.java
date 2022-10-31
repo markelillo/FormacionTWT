48089b8d7469796d4d7cf9c4a1776c0305b0876c
osmotor <markeletxebarria77@gmail.com>
2022-10-31 13:59:25 +0100

    comentarios

--- Proy010_Spring_Core/src/main/java/com/curso/spring/practica001_1/ConfigurarBeansApp.java
+++ Proy010_Spring_Core/src/main/java/com/curso/spring/practica001_1/ConfigurarBeansApp.java
@@ -1,11 +1,16 @@
 package com.curso.spring.practica001_1;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 
 
 @Configuration
+//@ComponentScan(basePackages = "com.curso")
+//@ImportResource("classspath:misbeans.xml")
+//@Import(GranjaJavaConfig.class)
 public class ConfigurarBeansApp {
 	@Bean
 	public JefeEquipo jefe1() {
