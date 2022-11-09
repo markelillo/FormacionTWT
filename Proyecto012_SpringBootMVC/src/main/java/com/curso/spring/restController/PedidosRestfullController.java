fcdc709296bc8dd25be37bc98778d9c7af969440
osmotor <markeletxebarria77@gmail.com>
2022-11-09 11:42:00 +0100

    añadir @Cross

--- Proyecto012_SpringBootMVC/src/main/java/com/curso/spring/restController/PedidosRestfullController.java
+++ Proyecto012_SpringBootMVC/src/main/java/com/curso/spring/restController/PedidosRestfullController.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,14 @@ import com.curso.spring.repositorio.PedidoJPARepositoryImp;
 import com.curso.spring.servicios.PedidosService;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200" } , 
+methods = {RequestMethod.GET,
+		RequestMethod.POST, 
+		RequestMethod.DELETE, 
+		RequestMethod.PUT,
+		RequestMethod.OPTIONS
+})//permitir que puedas acceder desde el servidor 8080 añl 4200
+
 public class PedidosRestfullController {
 	private static Logger log = LoggerFactory.getLogger(PedidoJPARepositoryImp.class);
 
